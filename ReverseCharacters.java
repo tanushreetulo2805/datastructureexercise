c696e7b3f93d3e050b8a00f0679b7764d4795bb2
tanushreetulo2805 <70585342+tanushreetulo2805@users.noreply.github.com>
2020-11-17 13:29:31 +0530

    Update ReverseCharacters.java

--- ReverseCharacters.java
+++ ReverseCharacters.java
@@ -1,24 +1,28 @@
-import java.util.*;
-public class ReverseCharacters 
-{
-    public static void main(String args[])
-    {
-        String actualWord = "IBS-Software";
-        String reverseWord = reverseWord(actualWord);
-        System.out.println("The Reverse String of " + actualWord + " is " + reverseWord);
-        
-    }
-         static String reverseWord(String actualWord)
-        {
-            StringBuilder reverseStrBuilder = new StringBuilder();
-            char[] reverseChar = actualWord.toCharArray();
-            //System.out.println(reverseChar);
-            for (int i = reverseChar.length - 1; i >= 0; i--)
-            {
-                reverseStrBuilder.append(reverseChar[i]);
-                //System.out.println(reverseStrBuilder);
-            }
-            
-            return reverseStrBuilder.toString();
-        }
+/*
+ * 
+ * String Reversal using Iteration / Recursion - 
+ * Take sample String and print reverse of String characters.
+ * 
+ */
+package practice;
+
+public class ReverseCharacters {
+	public static void main(String args[]) {
+		String actualWord = "IBS-Software";
+		String reverseWord = reverseWord(actualWord);
+		System.out.println("The Reverse String of " + actualWord + " is: " + reverseWord);
+
+	}
+
+	static String reverseWord(String actualWord) {
+		StringBuilder reverseStrBuilder = new StringBuilder();
+		char[] reverseChar = actualWord.toCharArray();
+		// System.out.println(reverseChar);
+		for (int i = reverseChar.length - 1; i >= 0; i--) {
+			reverseStrBuilder.append(reverseChar[i]);
+			// System.out.println(reverseStrBuilder);
+		}
+
+		return reverseStrBuilder.toString();
+	}
 }
