c260cdf924eced0d94fdf24ece2793b0b6aa0db8
tanushreetulo2805 <70585342+tanushreetulo2805@users.noreply.github.com>
2020-11-17 13:31:49 +0530

    Update ReverseWords.java

--- ReverseWords.java
+++ ReverseWords.java
@@ -1,36 +1,36 @@
+/*
+ * 
+ * Reverse words in a sentence - Take sample sentence and print reverse words of sentence.
+ * 
+ */
+package practice;
+
 import java.util.*;
-public class ReverseWords
-{
-    public static void main(String[] args)
-    {
-        String inpString = "IBS Software PVT LTD";
-        reverseSentence(inpString);
-    }
-    
-    static void reverseSentence(String inpString)
-    {
-        Stack<String> sentenceStack = new Stack<>();
-        String temp = "";
-        for(int i = 0; i < inpString.length(); i++)
-        {
-            if(inpString.charAt(i) == ' ')
-            {
-                sentenceStack.add(temp);
-                temp = "";
-            }
-            else
-            {
-                temp = temp + inpString.charAt(i);
-            }
-        }
-        sentenceStack.add(temp);
-        while(!sentenceStack.isEmpty())
-        {
-            temp = sentenceStack.peek();
-            System.out.print(temp + " ");
-            sentenceStack.pop();
-            //System.out.println(sentenceStack);
-        }
-    }
-    
+
+public class ReverseWords {
+	public static void main(String[] args) {
+		String inpString = "IBS Software PVT LTD";
+		reverseSentence(inpString);
+	}
+
+	static void reverseSentence(String inpString) {
+		Stack<String> sentenceStack = new Stack<>();
+		String temp = "";
+		for (int i = 0; i < inpString.length(); i++) {
+			if (inpString.charAt(i) == ' ') {
+				sentenceStack.add(temp);
+				temp = "";
+			} else {
+				temp = temp + inpString.charAt(i);
+			}
+		}
+		sentenceStack.add(temp);
+		while (!sentenceStack.isEmpty()) {
+			temp = sentenceStack.peek();
+			System.out.print(temp + " ");
+			sentenceStack.pop();
+			// System.out.println(sentenceStack);
+		}
+	}
+
 }
